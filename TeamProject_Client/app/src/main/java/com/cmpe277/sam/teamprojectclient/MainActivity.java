d96def93477ac65e9e9d96f6efc555ddcc348f71
Wenjin Ma <xiaoshan1213@gmail.com>
2017-05-03 10:36:38 -0700

    client_init

---
+++ TeamProject_Client/app/src/main/java/com/cmpe277/sam/teamprojectclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cmpe277.sam.teamprojectclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
